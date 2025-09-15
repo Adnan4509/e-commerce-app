dd46e2018c705eeae8aa71cd9ceaf9827b61c198
Adnan4509 <adnanwebdev7@gmail.com>
2025-09-15 21:56:55 +0530

    added Dto's & mapper & custom exception & put mapping

--- services/customer/src/main/java/com/adnan/ecommerce/customer/CustomerController.java
+++ services/customer/src/main/java/com/adnan/ecommerce/customer/CustomerController.java
@@ -3,10 +3,7 @@ package com.adnan.ecommerce.customer;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/customer")
@@ -18,5 +15,11 @@ public class CustomerController {
     public ResponseEntity<String> createCustomer(@RequestBody
                                                      @Valid CustomerRequest request){
         return ResponseEntity.ok(service.createCustomer(request));
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateCustomer(@RequestBody @Valid CustomerRequest request){
+        service.updateCustomer(request);
+        return ResponseEntity.accepted().build();
     }
 }
--- services/customer/src/main/java/com/adnan/ecommerce/customer/CustomerService.java
+++ services/customer/src/main/java/com/adnan/ecommerce/customer/CustomerService.java
@@ -1,19 +1,47 @@
 package com.adnan.ecommerce.customer;
 
+import com.adnan.ecommerce.exception.CustomerNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
+
+import java.text.Format;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
 
     private final CustomerRepository customerRepo;
+    private final CustomerMapper mapper;
 
     public String createCustomer(@Valid CustomerRequest request) {
-        return customerRepo.save()
+        var customer = customerRepo.save(mapper.toCustomer(request));
+        return customer.getId();
     }
 
 
+    public void updateCustomer(CustomerRequest request) {
+        var customer = customerRepo.findById(request.id())
+                .orElseThrow(() -> new CustomerNotFoundException(
+                String.format("Cannot update customer:: No customer found with the provided ID :: %s" , request.id())
+        ));
+        mergeCustomer(customer, request);
+        customerRepo.save(customer);
+    }
+
+    private void mergeCustomer(Customer customer, CustomerRequest request) {
+        if (StringUtils.isNotBlank(request.firstname())){
+            customer.setFirstname(request.firstname());
+        }
+        if (StringUtils.isNotBlank(request.lastname())){
+            customer.setLastname(request.lastname());
+        }
+        if (StringUtils.isNotBlank(request.email())){
+            customer.setEmail(request.email());
+        }
+        if (request.address() != null){
+            customer.setAddress(request.address());
+        }
+    }
 }
