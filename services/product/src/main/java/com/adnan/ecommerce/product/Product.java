37f9a308d9f3f519e8342baa5f483b1bf0a92759
Adnan4509 <adnanwebdev7@gmail.com>
2025-09-17 21:40:44 +0530

    added product service & database configuration

---
+++ services/product/src/main/java/com/adnan/ecommerce/product/Product.java
@@ -0,0 +1,29 @@
+package com.adnan.ecommerce.product;
+
+import com.adnan.ecommerce.category.Category;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    private double availableQuantity;
+    private BigDecimal price;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
